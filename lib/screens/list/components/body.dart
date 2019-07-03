7a086100b2f637112a04f7f20b088913bde70d8d
Jason Haenlin <jasonjas5252@gmail.com>
2019-07-03 14:46:15 +0200

    Create a Card

--- lib/screens/list/components/body.dart
+++ lib/screens/list/components/body.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:otake_flutter_home/blocs/bloc_provider.dart';
 import 'package:otake_flutter_home/blocs/entry/entry_bloc.dart';
 import 'package:otake_flutter_home/models/entry_model.dart';
+import 'package:otake_flutter_home/screens/list/components/entry_card.dart';
 
 class Body extends StatelessWidget {
   const Body({Key key}) : super(key: key);
@@ -11,38 +12,22 @@ class Body extends StatelessWidget {
     final eventBloc = BlocProvider.of<EntryBloc>(context).entryBloc;
 
     return StreamBuilder<List<Entry>>(
-        stream: eventBloc.currentValue$,
-        builder: (BuildContext context, AsyncSnapshot<List<Entry>> snapshot) =>
-            snapshot.hasData
-                ? (ListView.builder(
-                    itemCount: snapshot.data.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return Container(
-                        height: 100.0,
-                        color: index % 2 == 1
-                            ? Colors.amber[600]
-                            : Colors.amber[400],
-                        child: Column(
-                          children: <Widget>[
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Text(
-                                'Entry ${snapshot.data[index].entry}',
-                                style: Theme.of(context).textTheme.display1,
-                              ),
-                            ),
-                            Text(
-                              'Description ${snapshot.data[index].description}',
-                              style: Theme.of(context).textTheme.subtitle,
-                            )
-                          ],
-                        ),
-                      );
-                    },
-                  ))
-                : Center(
-                    child: CircularProgressIndicator(
-                        backgroundColor: Colors.amber),
-                  ));
+      stream: eventBloc.currentValue$,
+      builder: (BuildContext context, AsyncSnapshot<List<Entry>> snapshot) =>
+          snapshot.hasData
+              ? (ListView.builder(
+                  itemCount: snapshot.data.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Container(
+                      height: 100.0,
+                      color: index % 2 == 1
+                          ? Colors.amber[600]
+                          : Colors.amber[400],
+                      child: EntryCard(data: snapshot.data[index]),
+                    );
+                  },
+                ))
+              : Center(child: CircularProgressIndicator()),
+    );
   }
 }
